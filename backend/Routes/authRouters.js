756d004c6ec598dd1211684fed187c54f2adabe6
madhusankha90 <madhusankha.networking@gmail.com>
2024-07-31 19:56:52 +0530

    update routers

---
+++ backend/Routes/authRouters.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/login', (req,res)=>{
+    res.send('Login Success')
+});
+router.post('/signup', (req,res)=>{
+    res.send('SignUp Success')
+});
+
+
+
+module.exports = router;
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const router = require('./Routes/authRouters')
+
 require('dotenv').config();
-require('./Models/db')
+require('./Models/db');
 
 const PORT = process.env.PORT || 8080
 
 app.get('/home',(req,res)=> {
     res.send('new');
 })
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', router)
 
 app.listen(PORT, ()=> {
     console.log(`server running on port ${PORT}`);
